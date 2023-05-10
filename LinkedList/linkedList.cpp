d9cb4f3d17b123d280a4cb85d2113832ae4d42df
saurabh-kud <saurabh322001raj@gmail.com>
2023-05-10 22:20:44 +0530

    singly linkedList done

--- LinkedList/linkedList.cpp
+++ LinkedList/linkedList.cpp
@@ -29,6 +29,10 @@ public:
     void deleteLast();
     void deleteFirst();
     void deleteAtPos(int);
+    void insertFirst(int);
+    void insertLast(int);
+    void insertAtPos(int, int);
+    void search(int);
 };
 
 linkedList::linkedList()
@@ -172,6 +176,105 @@ void linkedList::deleteAtPos(int pos){
 }
 
 
+void linkedList::insertFirst(int data){
+    node *newNode = new node(data);
+    if(head==NULL){
+        head = newNode;
+        tail = newNode;
+        cout << "item inserted into list with data " << data << endl;
+    }else{
+        node *temp = head;
+        head = newNode;
+        head->next = temp;
+
+        cout << "item inserted into list with data " << data << endl;
+        
+    }
+}
+
+void linkedList::insertLast(int data){
+    node *newNode = new node(data);
+
+    if(head==NULL){
+        head = newNode;
+        tail = newNode;
+        cout << "item inserted into list with data " << data << endl;
+
+    }else{
+        node *temp = head;
+        while(temp->next!=NULL){
+        temp = temp->next;
+
+        }
+        temp->next = newNode;
+        tail = newNode;
+        cout << "item inserted into list with data " << data << endl;
+
+    }
+}
+
+void linkedList::insertAtPos(int pos,int data){
+     if(head==NULL && pos>0){
+        cout << "operation can't perform it's because list is empty and pos is greater than 0" << endl;
+     }else if(head==NULL && pos==0){
+        node *newNode = new node(data);
+        head = newNode;
+        tail = newNode;
+        cout << "item inserted into list with data " << data << endl;
+     }else{
+
+        node *newNode = new node(data);
+
+        int tempPos = 1;
+       if(pos<0){
+        cout << "operation can't be perform it's because invalid position " << endl;
+       }else if(pos==0){
+        newNode->next = head;
+        head = newNode;
+        cout << "item inserted into list with data " << data << endl;
+       }else{
+        node *temp = head;
+        while (tempPos <= pos-1){
+            temp = temp->next;
+            tempPos++;
+            if(temp==NULL){
+                break;
+            }
+         }
+         if(temp==NULL){
+            cout << "invalid position (may be greater than linkedlist )" << endl;
+            return;
+         }
+         node *tempNode = temp->next;
+         temp->next = newNode;
+         newNode->next = tempNode;
+        cout << "item inserted into list with data " << data << endl;
+
+       }
+
+     }
+}
+
+void linkedList::search(int item){
+    if(head==NULL){
+       cout << "list is empty " << endl;
+    }else{
+       node *temp = head;
+       int pos = 0;
+       while(temp!=NULL){
+         if(temp->data==item){
+            cout << "item found in the list at " << pos << " location" << endl;
+            break;
+         }
+           temp = temp->next;
+            pos++;
+       }
+       if(temp==NULL){
+            cout << "item not available into list" << endl;
+       }
+    }
+}
+
 int main(){
     linkedList l1;
     l1.insert();
@@ -190,6 +293,12 @@ int main(){
          << "deleting from pos " << endl;
     l1.deleteAtPos(2);
     l1.print();
+    l1.insertFirst(56);
+    l1.insertLast(2);
+    l1.print();
+    l1.insertAtPos(4, 100);
+    l1.print();
+    l1.search(202);
 
     return 0;
 }
